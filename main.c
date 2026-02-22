8d9ee8ab243a38ffa51d42e7f3899c68a26ecc2e
chrid97 <cgoncal12@gmail.com>
2026-02-21 18:47:09 -0800

    Rework tiling

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-// #include "raylib.h"
+#include "raylib.h"
 #include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -7,160 +7,108 @@
 #include <string.h>
 #include <time.h>
 
-typedef enum {
-  A_WORDS,
-  B_WORDS,
-  C_WORDS,
-  D_WORDS,
-  E_WORDS,
-  F_WORDS,
-  G_WORDS,
-  H_WORDS,
-  I_WORDS,
-  J_WORDS,
-  K_WORDS,
-  L_WORDS,
-  M_WORDS,
-  N_WORDS,
-  O_WORDS,
-  P_WORDS,
-  Q_WORDS,
-  R_WORDS,
-  S_WORDS,
-  T_WORDS,
-  U_WORDS,
-  V_WORDS,
-  W_WORDS,
-  X_WORDS,
-  Y_WORDS,
-  Z_WORDS,
-  WORD_TYPE_COUNT
-} WordType;
+#define VIRTUAL_WIDTH 640
+#define VIRTUAL_HEIGHT 360
 
+//----------------------------------------------------------------------------------
+// Types
+//----------------------------------------------------------------------------------
+typedef enum { TILE } EntityType;
+typedef enum { HAND, BAG, USED, IN_PLAY } TileLocation;
+
+typedef struct {
+  EntityType type;
+  Vector2 position;
+  int width;
+  int height;
+
+  char tile_value;
+  bool tile_location;
+} Entity;
+
+#define HAND_SIZE 16
+typedef struct {
+  Entity index[HAND_SIZE]; // index into letter bag
+  uint8_t size;
+} Hand;
+
+#define LETTER_BAG_SIZE 100
+typedef struct {
+  Entity tiles[LETTER_BAG_SIZE];
+  uint8_t size;
+} LetterBag;
+//----------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------
+// State
+//----------------------------------------------------------------------------------
 char *words;
-int file_size;
 int total_words = 0;
-int word_score = 0;
 
-char letter_bag[100] = {0};
-uint8_t remaining_letters = 0;
-char hand[15] = {0};
+Entity letter_bag[LETTER_BAG_SIZE] = {0};
+//----------------------------------------------------------------------------------
 
+//----------------------------------------------------------------------------------
+// Functions
+//----------------------------------------------------------------------------------
 void init_letter_bag() {
+  uint8_t letter_occurrence[26] = {
+      8,  // a
+      2,  // b
+      4,  // c
+      4,  // d
+      11, // e
+      1,  // f
+      3,  // g
+      2,  // h
+      8,  // i
+      1,  // j
+      1,  // k
+      5,  // l
+      3,  // m
+      7,  // n
+      6,  // o
+      3,  // p
+      1,  // q
+      7,  // r
+      8,  // s
+      5,  // t
+      4,  // u
+      1,  // v
+      1,  // w
+      1,  // x
+      2,  // y
+      1   // z
+  };
+  Entity entity = {.position = {}, .type = TILE};
+
   int i = 0;
-  // a: 8
-  for (int j = 0; j < 8; j++)
-    letter_bag[i++] = 'a';
+  for (int letter = 0; letter < 26; letter++) {
+    for (int j = 0; j < letter_occurrence[letter]; j++) {
+      letter_bag[i++] = (Entity){
+          .type = TILE,
+          .tile_location = BAG,
+          .position = {},
+          .height = 50,
+          .width = 50,
+          .tile_value = 97 + letter,
+      };
+    }
+  }
+}
 
-  // b: 2
-  for (int j = 0; j < 2; j++)
-    letter_bag[i++] = 'b';
-
-  // c: 4
-  for (int j = 0; j < 4; j++)
-    letter_bag[i++] = 'c';
-
-  // d: 4
-  for (int j = 0; j < 4; j++)
-    letter_bag[i++] = 'd';
-
-  // e: 11
-  for (int j = 0; j < 11; j++)
-    letter_bag[i++] = 'e';
-
-  // f: 1
-  letter_bag[i++] = 'f';
-
-  // g: 3
-  for (int j = 0; j < 3; j++)
-    letter_bag[i++] = 'g';
-
-  // h: 2
-  for (int j = 0; j < 2; j++)
-    letter_bag[i++] = 'h';
-
-  // i: 8
-  for (int j = 0; j < 8; j++)
-    letter_bag[i++] = 'i';
-
-  // j: 1
-  letter_bag[i++] = 'j';
-
-  // k: 1
-  letter_bag[i++] = 'k';
-
-  // l: 5
-  for (int j = 0; j < 5; j++)
-    letter_bag[i++] = 'l';
-
-  // m: 3
-  for (int j = 0; j < 3; j++)
-    letter_bag[i++] = 'm';
-
-  // n: 7
-  for (int j = 0; j < 7; j++)
-    letter_bag[i++] = 'n';
-
-  // o: 6
-  for (int j = 0; j < 6; j++)
-    letter_bag[i++] = 'o';
-
-  // p: 3
-  for (int j = 0; j < 3; j++)
-    letter_bag[i++] = 'p';
-
-  // q: 1
-  letter_bag[i++] = 'q';
-
-  // r: 7
-  for (int j = 0; j < 7; j++)
-    letter_bag[i++] = 'r';
-
-  // s: 8
-  for (int j = 0; j < 8; j++)
-    letter_bag[i++] = 's';
-
-  // t: 5
-  for (int j = 0; j < 5; j++)
-    letter_bag[i++] = 't';
-
-  // u: 4
-  for (int j = 0; j < 4; j++)
-    letter_bag[i++] = 'u';
-
-  // v: 1
-  letter_bag[i++] = 'v';
-
-  // w: 1
-  letter_bag[i++] = 'w';
-
-  // x: 1
-  letter_bag[i++] = 'x';
-
-  // y: 2
-  for (int j = 0; j < 2; j++)
-    letter_bag[i++] = 'y';
-
-  // z: 1
-  letter_bag[i++] = 'z';
+void draw_hand(Hand *hand) {
+  for (int i = 0; i < hand->size; i++) {
+    // hand->tiles[i] = ;
+  }
 }
 
 void shuffle_bag() {
   for (int i = 99; i > 0; i--) {
     int j = rand() % (i + 1);
-    char temp = letter_bag[i];
+    Entity temp = letter_bag[i];
     letter_bag[i] = letter_bag[j];
     letter_bag[j] = temp;
-  }
-}
-
-void draw_hand() {
-  for (int i = 0; i < 16; i++) {
-    if (letter_bag[i] == -1) {
-      continue;
-    }
-    hand[i] = letter_bag[i];
-    letter_bag[i] = -1;
   }
 }
 
@@ -174,7 +122,7 @@ void load_words() {
 
   // handle fseek error
   fseek(file, 0, SEEK_END);
-  file_size = ftell(file);
+  int file_size = ftell(file);
   fseek(file, 0, SEEK_SET);
 
   words = malloc(file_size + 1);
@@ -192,44 +140,37 @@ void load_words() {
 }
 
 int main(int argc, char *argv[]) {
-  load_words();
+  InitWindow(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, "Wordgame");
+  SetTargetFPS(0);
 
+  load_words();
   srand(time(NULL));
   init_letter_bag();
   shuffle_bag();
-  draw_hand();
 
-  for (int i = 0; i < 16; i++) {
-    if (i % 4 == 0) {
-      printf("\n");
+  while (!WindowShouldClose()) {
+    // Update
+    //----------------------------------------------------------------------------------
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      Vector2 mouse_pos = GetMousePosition();
+      // printf("%d", mouse_pos.x);
+      // printf("%d", mouse_pos.y);
     }
-    printf("%c ", hand[i]);
-  }
-  printf("\n");
+    //----------------------------------------------------------------------------------
 
-  printf("Type word: \n");
-  char user_input[12];
-  scanf("%11s", user_input);
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+    ClearBackground(ORANGE);
 
-  char *p = words;
-
-  bool match_found = false;
-
-  for (int i = 0; i < total_words; i++) {
-    if (strcmp(p, user_input) == 0) {
-      int damage = strlen(p);
-      printf("You deal %i damage\n", damage);
-      match_found = true;
-      break;
-    }
-    p += strlen(p) + 1;
+    EndDrawing();
+    //----------------------------------------------------------------------------------
   }
 
-  if (match_found) {
-    printf("Match found\n");
-  } else {
-    printf("Match not found\n");
-  }
+  // De-Initialization
+  //--------------------------------------------------------------------------------------
+  CloseWindow(); // Close window and OpenGL context
+  //--------------------------------------------------------------------------------------
 
   return 0;
 }
