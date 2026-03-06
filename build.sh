e339b6442bcda4f0ca55a6ec944d967042fc61d1
Christopher Goncalves <cgoncal12@gmail.com>
2026-03-05 19:30:22 -0800

    Add macos build

--- build.sh
+++ build.sh
@@ -1,10 +1,24 @@
+#!/bin/bash
 mkdir -p build/
+# Ensure raylib is built for system platform
+if [ ! -f ./lib/raylib/src/libraylib.a ]; then
+  echo "📦 Building Raylib library..."
+  (cd lib/raylib/src && make PLATFORM=PLATFORM_DESKTOP RAYLIB_LIBTYPE=STATIC)
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  clang ./main.c \
+    -I./lib/raylib/src/ \
+    ./lib/raylib/src/libraylib.a \
+    -framework Cocoa -framework IOKit -framework CoreAudio -framework CoreVideo -framework OpenGL \
+    -o ./build/wordgame.exe
+else
+  gcc ./main.c \
+    -I./lib/raylib/src/ \
+    ./lib/raylib/src/libraylib.a \
+    -lglfw -lGL -lopenal -lm -lpthread -ldl -lrt -lX11 \
+    -o ./build/wordgame.exe
+fi
 
 # (TODO) Add release build flags
 # -Wall -Wextra -Wpedantic -O0
-
-gcc ./main.c \
-  -I./lib/raylib/src/ \
-  ./lib/raylib/src/libraylib.a \
-  -lglfw -lGL -lopenal -lm -lpthread -ldl -lrt -lX11 \
-  -o ./build/wordgame.exe
